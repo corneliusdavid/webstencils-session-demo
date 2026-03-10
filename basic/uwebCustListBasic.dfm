79500ce505e3740061676c4b1f4ce112e6849e2a
David Cornelius <dev@corneliusconcepts.com>
2026-03-09 20:17:37 -0700

    trying to get basic authentication working

---
+++ basic/uwebCustListBasic.dfm
@@ -0,0 +1,77 @@
+object webCustListBasic: TwebCustListBasic
+  OnCreate = WebModuleCreate
+  Actions = <>
+  Height = 339
+  Width = 279
+  object wsEngineCustList: TWebStencilsEngine
+    Dispatcher = WebFileDispatcher
+    PathTemplates = <>
+    RootDirectory = '/'
+    OnError = wsEngineCustListError
+    Left = 64
+    Top = 32
+  end
+  object WebBasicAuthenticator: TWebBasicAuthenticator
+    LogoutURL = '/logout'
+    OnAuthenticate = WebBasicAuthenticatorAuthenticate
+    OnAuthenticated = WebBasicAuthenticatorAuthenticated
+    Left = 136
+    Top = 152
+  end
+  object WebAuthorizer: TWebAuthorizer
+    UnauthorizedURL = '/notallowed'
+    Zones = <
+      item
+        PathInfo = '/'
+        Kind = zkFree
+      end
+      item
+        PathInfo = '/custlist'
+        Roles = 'viewer'
+      end>
+    Left = 160
+    Top = 208
+  end
+  object WebFileDispatcher: TWebFileDispatcher
+    WebFileExtensions = <
+      item
+        MimeType = 'text/css'
+        Extensions = 'css'
+      end
+      item
+        MimeType = 'text/html'
+        Extensions = 'html;htm'
+      end
+      item
+        MimeType = 'application/javascript'
+        Extensions = 'js'
+      end
+      item
+        MimeType = 'image/jpeg'
+        Extensions = 'jpeg;jpg'
+      end
+      item
+        MimeType = 'image/png'
+        Extensions = 'png'
+      end>
+    BeforeDispatch = WebFileDispatcherBeforeDispatch
+    WebDirectories = <
+      item
+        DirectoryAction = dirInclude
+        DirectoryMask = '*'
+      end
+      item
+        DirectoryAction = dirExclude
+        DirectoryMask = '\html\*'
+      end>
+    RootDirectory = '/html'
+    VirtualPath = '/'
+    DefaultFile = 'index.html'
+    Left = 80
+    Top = 256
+  end
+  object WebSessionManager: TWebSessionManager
+    Left = 112
+    Top = 96
+  end
+end
---
+++ basic/uwebCustListBasic.pas
@@ -0,0 +1,166 @@
+unit uwebCustListBasic;
+
+{$IF CompilerVersion < 37.0}
+  {$MESSAGE FATAL 'Delphi 13 or newer required for this project.'}
+{$ENDIF}
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Web.HTTPApp, Web.HTTPProd, Web.Stencils;
+
+type
+  TwebCustListBasic = class(TWebModule)
+    wsEngineCustList: TWebStencilsEngine;
+    WebBasicAuthenticator: TWebBasicAuthenticator;
+    WebAuthorizer: TWebAuthorizer;
+    WebFileDispatcher: TWebFileDispatcher;
+    WebSessionManager: TWebSessionManager;
+    procedure WebModuleCreate(Sender: TObject);
+    procedure wsEngineCustListError(Sender: TObject; const AMessage: string);
+    procedure WebBasicAuthenticatorAuthenticate(
+      Sender: TCustomWebAuthenticator; Request: TWebRequest; const UserName,
+      Password: string; var Roles: string; var Success: Boolean);
+    procedure WebBasicAuthenticatorAuthenticated(Sender: TCustomWebAuthenticator;
+        Request: TWebRequest; const UserName: string; var Roles: string);
+    procedure WebFileDispatcherBeforeDispatch(Sender: TObject; const AFileName:
+        string; Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+  private
+    // these are NOT accessible by the WebStencilsEngine
+    FTitle: string;
+    FVersion: string;
+    FFullName: string;
+    FUserTitle: string;
+    FHighestUserRole: string;
+    procedure ClearVars;
+    procedure OpenCustomerList;
+    procedure EditCustomer(const CustParams: string);
+  public
+    // these will be available by the WebStencilsEngine parser
+    property Title: string read FTitle;
+    property Version: string read FVersion;
+    property FullName: string read FFullName;
+    property UserTitle: string read FUserTitle;
+    property HighestUserRole: string read FHighestUserRole write FHighestUserRole;
+  end;
+
+var
+  WebModuleClass: TComponentClass = TwebCustListBasic;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+uses
+  Dialogs, System.IOUtils, System.bindings.EvalProtocol, System.Bindings.Methods,
+  udmCust, uLogging;
+
+procedure TwebCustListBasic.WebModuleCreate(Sender: TObject);
+begin
+  // web app vars
+  FTitle := 'Customer List for WebStencils with Basic Authentication';
+  FVersion := '0.6';
+
+  wsEngineCustList.AddVar('App', Self, False);
+
+  // html paths
+  wsEngineCustList.RootDirectory := TPath.Combine(ExtractFilePath(ParamStr(0)), 'html');
+  for var i := 0 to ComponentCount - 1 do begin
+    if Components[i] is TWebStencilsProcessor then
+      TWebStencilsProcessor(Components[i]).InputFileName := TPath.Combine(
+        wsEngineCustList.RootDirectory, TWebStencilsProcessor(Components[i]).InputFileName);
+  end;
+end;
+
+procedure TwebCustListBasic.ClearVars;
+begin
+  FFullName := EmptyStr;
+  FUserTitle := EmptyStr;
+  FHighestUserRole := EmptyStr;
+end;
+
+procedure TwebCustListBasic.OpenCustomerList;
+begin
+  dmCust.qryCustomers.Close;
+  dmCust.qryCustomers.Open;
+  if not wsEngineCustList.HasVar('CustList') then
+    wsEngineCustList.AddVar('CustList', dmCust.qryCustomers, False);
+end;
+
+procedure TwebCustListBasic.EditCustomer(const CustParams: string);
+var
+  ParamsList: TStringList;
+  CustNo: string;
+  CustNum: Integer;
+begin
+  ParamsList := TStringList.Create;
+  try
+    ParamsList.CommaText := CustParams;
+  finally
+    ParamsList.Free;
+    if ParamsList.Count > 0 then
+    begin
+      CustNo := ParamsList.Values['cust_no'];
+      if TryStrToInt(CustNo, CustNum) then
+      begin
+        dmCust.OpenCustDetails(CustNum);
+        if not wsEngineCustList.HasVar('CustDetails') then
+          wsEngineCustList.AddVar('CustDetails', dmCust.qryCustDetails, False);
+      end;
+    end;
+  end;
+end;
+
+procedure TwebCustListBasic.WebBasicAuthenticatorAuthenticate(
+  Sender: TCustomWebAuthenticator; Request: TWebRequest; const UserName,
+  Password: string; var Roles: string; var Success: Boolean);
+begin
+  WebLogger.Add('WebBasicAuthenticatorAuthenticate.Request.PathInfo: ' + Request.PathInfo);
+
+  ClearVars;
+  Roles := EmptyStr;
+
+  Success := dmCust.LoginCheck(Username, Password);
+  if Success then begin
+    FFullName := dmCust.EmployeeFirstName + ' ' + dmCust.EmployeeLastName;
+    FUserTitle := dmCust.EmployeeTitle;
+    if Pos('MANAGER', UpperCase(FUserTitle)) > 0 then begin
+      Roles := 'viewer,editor,manager';
+      FHighestUserRole := 'manager';
+    end
+    else if (Pos('IT', UpperCase(FUserTitle)) > 0) then begin
+      Roles := 'viewer,editor';
+      FHighestUserRole := 'editor';
+    end else begin
+      Roles := 'viewer';
+      FHighestUserRole := 'viewer';
+    end;
+  end;
+end;
+
+procedure TwebCustListBasic.WebBasicAuthenticatorAuthenticated(Sender:
+    TCustomWebAuthenticator; Request: TWebRequest; const UserName: string; var
+    Roles: string);
+begin
+  WebLogger.Add('WebBasicAuthenticatorAuthenticated.Request.PathInfo: ' + Request.PathInfo);
+end;
+
+procedure TwebCustListBasic.WebFileDispatcherBeforeDispatch(Sender: TObject;
+    const AFileName: string; Request: TWebRequest; Response: TWebResponse; var
+    Handled: Boolean);
+begin
+  if SameText(Request.PathInfo, '/custlist') then
+    OpenCustomerList
+  else if Request.PathInfo.StartsWith('/custedit', True) then
+    EditCustomer(Request.QueryFields.ToString);
+end;
+
+procedure TwebCustListBasic.wsEngineCustListError(Sender: TObject; const AMessage: string);
+begin
+  WebLogger.Add('WebEngine ERROR: ' + AMessage);
+  ShowMessage(AMessage);
+end;
+
+end.
